2511030aa047d93b97165530551f518c8db9f304
Sachin <sachinacharya@SSA-MacBook-Pro.local>
2023-08-28 10:26:55 +0530

    conflict resolve

--- lib/routes/routes.dart
+++ lib/routes/routes.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import '../../../ui/splash/splashScreen.dart';
+import '../controller/bloc/signup/signup_bloc.dart';
 import '../services/rest_api/client.dart';
 import '../ui/home_screen/home_page.dart';
 import '../ui/login_intro/login_intro.dart';
@@ -15,15 +16,12 @@ class AppRoutes {
     switch (setting.name) {
       case "/":
         return MaterialPageRoute(
-            builder: (context) => BlocProvider(
-              create: (context)=>SignUpBloc(),
-              child: SignUp(
-              ),
+            builder: (context) => const SplashScreen(
             ));
       case LoginIntro.routeName:
         return MaterialPageRoute(builder: (context) => const LoginIntro());
         case HomePage.routeName:
-        return MaterialPageRoute(builder: (context) =>  HomePage());
+        return MaterialPageRoute(builder: (context) =>  const HomePage());
       default:
         return MaterialPageRoute(builder: (context) => const SplashScreen());
     }
